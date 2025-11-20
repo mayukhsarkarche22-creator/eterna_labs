f1f49184cf892d459101fc88cf2ce707deb23166
Mayukh_Sarkar <mayukh.sarkar.che22@itbhu.ac.in>
2025-11-20 20:05:55 +0530

    Redis connection update for deployment on the web

--- src/infrastructure/redis.ts
+++ src/infrastructure/redis.ts
@@ -1,10 +1,13 @@
 import Redis from 'ioredis';
 
-const redisConfig = {
-    host: process.env.REDIS_HOST || 'localhost',
-    port: Number(process.env.REDIS_PORT) || 6379,
-    maxRetriesPerRequest: null, // Required for BullMQ
-};
+export const redisConfig = {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD,
+    tls: process.env.REDIS_TLS ? { rejectUnauthorized: false } : undefined,
+    maxRetriesPerRequest: null,
+  };
+  console.log(redisConfig);
 
 // Shared Redis connection for general use
 export const redis = new Redis(redisConfig);
